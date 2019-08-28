e46e9dab460c37ce09b4d428f6ae0195074f6b7d
gtheofilo <p16034@students.cs.unipi.gr>
2019-08-28 17:00:04 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,39 +5,9 @@ import math
 from collections import Counter
 
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-
-def color_print(string, color_code, *args):
-    """Custom color printing for CLI"""
-    if len(args) > 0:
-        print(color_code, string, bcolors.ENDC, end=args[0])
-    else:
-        print(color_code, string, bcolors.ENDC)
-
-
-def print_title(string, *args):
-    color_print(string, bcolors.OKBLUE, *args)
-
-
-def print_process(string, *args):
-    color_print(string, bcolors.OKGREEN, *args)
-
-
 def character_frequencies(text):
-    """Removes all spaces and calculates the occuring frequency of each
+    """Calculates the occuring frequency of each
     caracter"""
-
-    # text = text.replace(' ', '')
     number_of_characters = len(text)
     c = Counter(text)
 
@@ -48,42 +18,23 @@ def character_frequencies(text):
     return frequencies
 
 
-def xor(a, b):
-    result = []
-    for i in range(1, len(b)):
-        if a[i] == b[i]:
-            result.append('0')
-        else:
-            result.append('1')
-    return ''.join(result)
-
-
-def mod2div(divident, divisor):
-    pick = len(divisor)
-    tmp = divident[0: pick]
-    while pick < len(divident):
-        if tmp[0] == '1':
-            tmp = xor(divisor, tmp) + divident[pick]
-        else:
-            tmp = xor('0' * pick, tmp) + divident[pick]
-        pick += 1
-    if tmp[0] == '1':
-        tmp = xor(divisor, tmp)
-    else:
-        tmp = xor('0' * pick, tmp)
-    checkword = tmp
-    return checkword
-
-
-def encodeData(data, key):
-    l_key = len(key)
-    appended_data = data + '0' * (l_key - 1)
-    remainder = mod2div(appended_data, key)
-    codeword = data + remainder
-    return codeword
-
-
 def shannon_fano(list_of_characters, encoded=[], prefix=''):
+    """An implementation of the shannon fano encoding using recursion
+
+    Args:
+    list_of_characters: A list of tuples with the occuring characters and
+    their frequency. Example: [(a, 0.3), (b, 0.3), (c, 0.3)]
+
+    encoded: The list that is used as memory to hold the encoding characters.
+    It is updated after each run of the algorithm
+
+    prefix: Like the encoded argument it is updated at each step of the
+    algorithm
+
+    Returns:
+        A dictionary variable with keys the characters and values their
+        encoded string
+    """
     frequencies_sum = 0
     isFirstIteration = True
 
@@ -128,46 +79,117 @@ def shannon_fano(list_of_characters, encoded=[], prefix=''):
 
 
 def compression(text, coding_dictionary):
-    # text = text.replace(' ', '')
+    "Calculates the encoded string for each character of the text file"
     encoded_text = []
     encoded_word = ''
     text = text + ' '
+
     for character in text:
         encoded_word = encoded_word + coding_dictionary[character]
         if character == ' ':
             encoded_text.append(encoded_word)
+            encoded_text.append(coding_dictionary[' '])
             encoded_word = ''
+
+    encoded_text.pop()
 
     return encoded_text
 
 
-filename = input('Provide the filename to be compressed: ')
-ext='.txt'
-filename+=ext
-f=open(filename,"r")
-if f.mode=="r":
-    text=f.read()
+def entropy_calculator(character_frequencies):
+    """Calculates the entropy given the frequencies of the characters"""
+    entropy = 0
+    for i in character_frequencies:
+        entropy = entropy + i[1] * math.log(1 / i[1], 2)
 
-text_size = len(text) * 8
+    return entropy
+
+def xor(a, b):
+    """Computes the XOR operation between A and B"""
+    result = []
+    for i in range(1, len(b)):
+        if a[i] == b[i]:
+            result.append('0')
+        else:
+            result.append('1')
+    return ''.join(result)
+
+
+def mod2div(divident, divisor):
+    pick = len(divisor)
+    tmp = divident[0: pick]
+    while pick < len(divident):
+        if tmp[0] == '1':
+            tmp = xor(divisor, tmp) + divident[pick]
+        else:
+            tmp = xor('0' * pick, tmp) + divident[pick]
+        pick += 1
+    if tmp[0] == '1':
+        tmp = xor(divisor, tmp)
+    else:
+        tmp = xor('0' * pick, tmp)
+    checkword = tmp
+    return checkword
+
+
+def encodeData(data, key):
+    l_key = len(key)
+    appended_data = data + '0' * (l_key - 1)
+    remainder = mod2div(appended_data, key)
+    codeword = data + remainder
+    return codeword
+
+
+class colors:
+    """Colors for printing the results"""""
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+
+    ENDC = '\033[0m'
+
+
+def color_print(string, color_code, *args):
+    """Custom color printing for CLI"""
+    if len(args) > 0:
+        print(color_code, string, colors.ENDC, end=args[0])
+    else:
+        print(color_code, string, colors.ENDC)
+
+
+def print_title(string, *args):
+    color_print(string, colors.OKBLUE, *args)
+
+
+def print_process(string, *args):
+    color_print(string, colors.OKGREEN, *args)
+
+
+
+
+# Configuration of the Python Script using text file's name, noise level and
+# code lenght
+filename = input('Provide the filename to be compressed(must be in the same '
+                 'folder with the .py file): ')
+filename = filename + '.txt'
+file = open(filename, 'r')
+if file.mode == 'r':
+    text_file = file.read()
+
+text_size = len(text_file) * 8
 length = int(input('Provide the code length: '))
-
 noise = int(input('Provide the noise level: '))
 
-
+# First Question
 print_process('1. Generating the code table...', '')
-code_table = shannon_fano(character_frequencies(text))
+char_freq = character_frequencies(text_file)
+code_table = shannon_fano(char_freq)
 print_process('✓\n')
 print_process(f'Code Table: {code_table}', '\n\n')
 
-entropy = 0
-freq = character_frequencies(text)
-for i in character_frequencies(freq):
-    entropy = entropy + i[1] * math.log(1/i[1], 2)
-print(entropy)
 
-
+# Second Question
 print_process('2. Compressing the input...', '')
-compressed_text = compression(text, code_table)
+compressed_text = compression(text_file, code_table)
 print_process('✓\n')
 print_process(f'Compressed Text: {compressed_text}', '\n\n')
 comp_bits = 0
@@ -176,6 +198,8 @@ for i in compressed_text:
         comp_bits = comp_bits + 1
 
 key = '1001'  # g(x) synarthsh pou ginetai h diairesh
+
+# Third Question
 print_process('3. Applying the cyclic encoding...', '')
 cyclic_encoded = []
 for word in compressed_text:
@@ -187,8 +211,9 @@ for i in cyclic_encoded:
     for bit in i:
         added_bits = added_bits + 1
 
+# Fourth Question
 code_with_noise = []
-print_process('3. Applying noise...', '')
+print_process('4. Applying noise...', '')
 for word in cyclic_encoded:
     randint = random.randint(0, noise)
     temp = list(word)
@@ -202,13 +227,15 @@ for word in cyclic_encoded:
 print_process('✓\n')
 print_process(f'Noise Result: {code_with_noise}', '\n\n')
 
-print_process('4. Generating the Base64 string...', '')
+# Fifth Question
+print_process('5. Generating the Base64 string...', '')
 b_64 = base64.b64encode(
     ''.join(str(e) for e in compressed_text).encode('ascii'))
 print_process('✓\n')
 print_process(f'Base64 String: {b_64}', '\n\n')
 
-print_process('5. Generating the JSON...', '')
+# Sixth Question
+print_process('6. Generating the JSON...', '')
 j = {
 
     "compression_algorithm": "Fano-Shannon",
@@ -225,19 +252,19 @@ j = {
     }
 
 }
-j = json.dumps(j)
+j = json.dumps(j, indent=4)
 print_process('✓\n')
 
-
-print_process('6. Sending the message...', '')
+# Seventh Question
+print_process('7. Sending the message...', '')
 print_process('✓\n')
 print_process(f'JSON: {j}', '\n\n')
 
-compressed_size =5
-print_process('7. Generating statistics...', '')
+compressed_size = 5
+print_process('8. Generating statistics...', '')
 print_process('✓\n')
 print_process(f'Size: {text_size} bits', '\n\n')
 print_process(f'Compressed Size: {comp_bits} bits', '\n\n')
-print_process(f'Entropy: {entropy}', '\n\n')
+print_process(f'Final Size: {added_bits} bits', '\n\n')
+print_process(f'Entropy: {entropy_calculator(char_freq)}', '\n\n')
 print_process(f'Added Bits: {added_bits - comp_bits} bits', '\n\n')
-
